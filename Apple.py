51995674ed2213b23b4f282116ca7b1abd3ab268
Yusuf6256 <78488257+Yusuf6256@users.noreply.github.com>
2022-02-09 20:19:28 +0530

    Add files via upload

---
+++ Apple.py
@@ -0,0 +1,17 @@
+import cv2
+
+def take_snapshot():
+    videoCaptureObject = cv2.VideoCapture(0) 
+    result = True 
+    while(result): 
+        #read the frames while the camera is on 
+        ret,frame = videoCaptureObject.read() 
+        #cv2.imwrite() method is used to save an image to any storage device 
+        cv2.imwrite("NewPicture2.jpg",frame) 
+        result = False 
+        # releases the camera 
+        videoCaptureObject.release() 
+        #closes all the window that might be opened while this process 
+        cv2.destroyAllWindows() 
+    
+take_snapshot()
